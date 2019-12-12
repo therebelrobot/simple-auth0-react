964d6e8150716a6ecdbd17f2772a9bdd56c2c50f
Oz Haven (Trent Oswald) <ozzyhaven@therebelrobot.com>
2019-12-11 20:06:12 -0800

    initial version

---
+++ src/PrivateRoute.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import { useAuth0 } from './auth0';
+
+const PrivateRoute = ({ component: Component, path, ...rest }) => {
+  const { isAuthenticated, loginWithRedirect, loading } = useAuth0();
+
+  useEffect(() => {
+    if (loading) return;
+    const fn = async () => {
+      if (!isAuthenticated) {
+        await loginWithRedirect({
+          appState: { targetUrl: path }
+        });
+      }
+    };
+    fn();
+  }, [isAuthenticated, loginWithRedirect, path, loading]);
+
+  const render = props =>
+    isAuthenticated === true ? <Component {...props} /> : null;
+
+  return <Route path={path} render={render} {...rest} />;
+};
+
+export default PrivateRoute;
